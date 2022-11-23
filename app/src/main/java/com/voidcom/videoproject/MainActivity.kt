2bb57d51e20975a733c3f6cf14f86b9ac2639b85
voidcom <voidcom@163.com>
2022-11-23 19:15:43 +0800

    update gradle

--- app/src/main/java/com/voidcom/videoproject/MainActivity.kt
+++ app/src/main/java/com/voidcom/videoproject/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.voidcom.videoproject
 
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.view.View
 import androidx.activity.viewModels
 import com.voidcom.v_base.ui.BaseActivity
@@ -8,7 +10,6 @@ import com.voidcom.v_base.ui.EmptyViewModel
 import com.voidcom.videoproject.databinding.ActivityMainBinding
 import com.voidcom.videoproject.ui.OutputVideoInfoActivity
 import com.voidcom.videoproject.ui.VideoProcessActivity
-import com.voidcom.videoproject.ui.videoCut.VideoCutActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding, EmptyViewModel>(), View.OnClickListener {
     private val filePathList = arrayListOf<String>()
@@ -28,7 +29,15 @@ class MainActivity : BaseActivity<ActivityMainBinding, EmptyViewModel>(), View.O
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_test_cmd -> {
-                startActivity(Intent(this, VideoCutActivity::class.java))
+//                startActivity(Intent(this, VideoCutActivity::class.java))
+
+                val intent = Intent(Intent.ACTION_VIEW)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                }
+                intent.setDataAndType(Uri.parse("content://media/external/video/media/59"), "video/mp4")
+                startActivity(intent)
             }
             R.id.btn_output_file_info -> startActivity(
                 Intent(this, OutputVideoInfoActivity::class.java)
--- app/src/main/java/com/voidcom/videoproject/ui/OutputVideoInfoActivity.kt
+++ app/src/main/java/com/voidcom/videoproject/ui/OutputVideoInfoActivity.kt
@@ -4,15 +4,12 @@ import com.huantansheng.easyphotos.EasyPhotos
 import com.huantansheng.easyphotos.callback.SelectCallback
 import com.huantansheng.easyphotos.constant.Type
 import com.huantansheng.easyphotos.models.album.entity.Photo
-import com.voidcom.ffmpeglib.FFmpegCommand
 import com.voidcom.ffmpeglib.FFprobeCmd
 import com.voidcom.v_base.ui.BaseActivity
 import com.voidcom.v_base.ui.EmptyViewModel
 import com.voidcom.v_base.utils.KLog
 import com.voidcom.videoproject.databinding.ActivityOutputVideoInfoBinding
 import com.voidcom.videoproject.utils.GlideEngine
-import java.util.ArrayList
-import kotlin.concurrent.thread
 
 class OutputVideoInfoActivity : BaseActivity<ActivityOutputVideoInfoBinding, EmptyViewModel>() {
     private var filePath = ""
@@ -23,7 +20,6 @@ class OutputVideoInfoActivity : BaseActivity<ActivityOutputVideoInfoBinding, Emp
         super.onInitUI()
         mBinding.btnSelectFile.setOnClickListener {
             EasyPhotos.createAlbum(this, true, true, GlideEngine.newInstant)
-                .setFileProviderAuthority("com.example.demo.fileprovider")
                 .setCount(1)
                 .filter(Type.VIDEO)
                 .start(object : SelectCallback() {
